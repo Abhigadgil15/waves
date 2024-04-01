1cf1d9fa8f1aec85b9d11112eed8a22dace94930
Abhinav Gadgil <64949128+Abhigadgil15@users.noreply.github.com>
2024-04-01 18:25:06 +0530

    Update products.service.js

--- server/services/products.service.js
+++ server/services/products.service.js
@@ -1,6 +1,7 @@
 const { Product } = require('../models/product');
 const { ApiError } = require('../middleware/apiError');
 const httpStatus = require('http-status');
+const mongoose = require('mongoose');
 const addProduct = async(body) =>{
     try{
         const product = new Product({
@@ -83,21 +84,69 @@ const paginateProducts= async(req) =>{
         //need to do filtering
         // const example = {
         //     "keywords":"elite", // this is for search bar if user searches for word elite for example
-        //     "brand":["6609923e48d171e30308aa41"], // array of ids
-        //     "lt":200, // lesser than and greater than for filtering prices. There is a range tool n mongo db but not available for free tier
-        //     "gt":500,
+        //     "brand":["6609923e48d171e30308aa41"], // array of ids(this should be in pbject of type string)
+        //     "min":200, // lesser than and greater than for filtering prices. There is a range tool n mongo db but not available for free tier
+        //     "max":500,
         //     "frets":24 // check filter by 24
 
         //     //in real life this is going to go crazy for filtering
         // }
 
         //create an aggregate function from mongo db
-        let aggQueryArray = [];
+        let aggQueryArray = [
+            // {price},
+            // {keyword},
+            
+        ];
+        if(req.body.keywords && req.body.keywords!=''){
+            const re = new RegExp(`${req.body.keywords}`,'gi') // "g" means global and "i" is case insensitive
+            aggQueryArray.push({
+                $match:{model:{ $regex:re }} // searching for model in search bar. if user types iron anything with iron will be loaded
+            })
+        }
+        if(req.body.brand && req.body.brand.length > 0){
+            let newBrandsArray = req.body.brand.map((item)=>(
+                new mongoose.Types.ObjectId(item) // we use new keyword even if its deprecated because postman does not work
+            ));
+            aggQueryArray.push({
+                $match:{ brand:{ $in: newBrandsArray }} // input in BOody needs to be an array in postman
+            });
+        }
+        if(req.body.frets && req.body.frets.length > 0){
+            aggQueryArray.push({
+                $match:{frets: { $in : req.body.frets }} // input in Body needs to be an array in postman
+            })
+        }
+        if(req.body.min && req.body.min > 0 || req.body.max && req.body.max < 5000){
+            /// { $range: { price:[0,100]}} // not supported for free mongo db tier
+            if(req.body.min){
+                aggQueryArray.push({$match:{price:{$gt:req.body.min}}});
+                //minimum price, guitar with a price greaer than 
+            }
+            if(req.body.max){
+                aggQueryArray.push({$match:{price:{$lt:req.body.max}}});
+                //minimum price, guitar with a price greaer than 
+            }
+        }
+
+//// ADD POPULATE FOR BRANDS
+        aggQueryArray.push(
+            {$lookup: // to match a id with same id in collection
+            {
+                from:"brands",// collection name in mongo db
+                localField:"brand",// field name in collection
+                foreignField:"_id",//field that we are matching basically brand id both in products and brand collection
+                as: "brand"
+
+            }},
+            {$unwind:'$brand'} // this will return it in form of a object instead of an array
+        )
+
 
 
         let aggQuery = Product.aggregate(aggQueryArray);
         const options = {
-            page:req.body.page,
+            page:req.body.page, // test this in postman
             limit:2, //put limit to requests
             sort:{ date:'desc'} // you can add more options. check on aggregate-paginate documentation
         }
