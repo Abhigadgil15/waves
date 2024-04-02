f62b10cb9d21af8317856972abc7ba03d6d5e94d
Abhinav Gadgil <64949128+Abhigadgil15@users.noreply.github.com>
2024-04-03 02:28:58 +0530

    client update

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+// import Dashboard from "./dashboard/page"
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-          Hello
+          <h1 className="roboto">Hello</h1>
+          <h1 className="monoton">Hello</h1>
+          <h1>Hello</h1>
       </div>
     </main>
   );
